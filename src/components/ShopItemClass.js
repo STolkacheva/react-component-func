60a3155a9e7469be083e7a77277dd5e0c27c9fb9
s.tolkacheva <s.tolkacheva@systtech.ru>
2022-02-21 17:44:32 +0200

    Страница интернет-магазина (функциональный компонент)

---
+++ src/components/ShopItemClass.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {ShopItem} from "../Types";
+
+export default function ShopItemClass(props) {
+  const {item} = props
+
+  return (
+  <div class="main-content">
+    <h2>{item.brand}</h2>
+    <h1>{item.title}</h1>
+    <h3>{item.description}</h3>
+    <div class="description">{item.descriptionFull}</div>
+    <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
+    <div class="divider"></div>
+    <div class="purchase-info">
+      <div class="price">{item.currency}{item.price}</div>
+      <button>Добавить в корзину</button>
+    </div>
+  </div>
+  )
+}
+
+ShopItemClass.PropTypes = {
+  item: PropTypes.instanceOf(ShopItem).isRequired
+}
